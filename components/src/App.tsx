721bc814cc9cbd23421d893485a9f1f2d8893e09
rry <r.yamakuchi@gmail.com>
2023-11-24 00:46:39 +0900

    fix: React に変えた

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,32 +1,32 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = createSignal(0)
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
+      <h1>Vite + React</h1>
+      <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
+          count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
